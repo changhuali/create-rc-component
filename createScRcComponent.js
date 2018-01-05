aae1e17c6b791784c4fdf01a52ef50c77e3455c8
scdev <changhua.li@socialcredits.cn>
2018-01-05 13:49:30 +0800

    feat:building tool structure

---
+++ createScRcComponent.js
@@ -0,0 +1,155 @@
+/*
+*Create a user component dir.
+*This file must work on node 8+.
+*/
+
+'use strict';
+
+const colors = require('colors');
+const program = require('commander');
+const path = require('path');
+const validateCompName = require('validate-npm-package-name');
+const fs = require('fs-extra');
+const execSync = require('child_process').execSync;
+const semver = require('semver');
+const spawnSync = require('child_process').spawnSync;
+
+const packageJson = require('./package.json');
+
+const COMPONENT_NAME = '<component name>';
+const OPTIONS = '[options]';
+const ARGUMENTS = COMPONENT_NAME + ' ' + OPTIONS;
+const MIN_NPM_VERSION = '3.0.0';
+const ALL_DEPENDENCIES = ['react', 'react-dom'];
+
+let componentName;
+
+program
+  // .command(packageJson.name)
+  .version(packageJson.version)
+  .arguments(ARGUMENTS)
+  .usage(colors.green(ARGUMENTS))
+  .action((name) => {
+    componentName = name;
+  })
+  .parse(process.argv);
+
+if (typeof componentName === 'undefined') {
+  console.log(colors.cyan('-----------------------------------------------------------'));
+  console.error('Please specify the component name:');
+  console.log(`  ${colors.cyan(program.name())} ${colors.green(COMPONENT_NAME)}`);
+  console.log();
+  console.log('For example:');
+  console.log(`  ${colors.cyan(program.name())} ${colors.green('my-component')}`);
+  console.log();
+  console.log(`Run ${colors.cyan(`${program.name()} --help`)} to get help information.`);
+  console.log(colors.cyan('-----------------------------------------------------------'));
+  process.exit(1);
+}
+
+function printCheckResult(results) {
+  if (typeof results !== 'undefined') {
+    results.forEach(error => {
+      console.error(colors.red(`      ${error}`));
+    });
+  }
+}
+
+function checkCompName(compName) {
+  const checkResult = validateCompName(compName);
+  if (!checkResult.validForNewPackages) {
+    console.error(
+      `Error:Could not create a component called ${colors.red(compName)}
+      because of ${colors.green('npm naming restrictions')}:`
+    );
+    printCheckResult(checkResult.errors);
+    printCheckResult(checkResult.warnings);
+    process.exit(1);
+  }
+}
+
+function mkdirOfComp(root, compName) {
+  const isExist = fs.pathExistsSync(root);
+  if (isExist) {
+    console.error(
+      `${colors.red(`Error:the dir ${compName} has already exist,
+      try to using a new component name or remove the exist dir.`)}`
+    );
+    process.exit(1);
+  }
+  fs.ensureDirSync(root);
+  console.log(`Create a new component in ${colors.green(root)}`);
+}
+
+function createPackageJsonFile(root, compName) {
+  const packageJson = {
+    "name": compName,
+    "version": "1.0.0",
+    "description": "add some your description about this component",
+    "repository": {},
+    "keywords": [
+      "react",
+      "component"
+    ],
+    "license": "MIT",
+  };
+  fs.writeFileSync(
+    path.join(root, 'package.json'),
+    JSON.stringify(packageJson, null, 2)
+  );
+}
+
+function checkNpmValid(root) {
+  let npmVersion = null;
+  let npmVersionValid = false;
+  try {
+    npmVersion = execSync('npm --version')
+      .toString()
+      .trim();
+    npmVersionValid = semver.gte(npmVersion, MIN_NPM_VERSION);
+    if (!npmVersion) {
+      console.log(colors.error(
+        `Error:there is no npm in your environment.`
+      ));
+      fs.removeSync(root);
+      process.exit(1);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function installDependcies(root) {
+  console.log(colors.green('Installing dependencies. Wait a while.'));
+  const child = spawnSync('npm', ['install', '--save'].concat(ALL_DEPENDENCIES), {stdio: 'inherit'});
+  if (child.status !== 0) {
+    console.log(colors.red(
+      `Error:the command ${colors.cyan(`npm install ${ALL_DEPENDENCIES.join(' ')} --save`)} has failed.`
+    ));
+    fs.removeSync(root);
+    process.exit(1);
+  } else {
+    console.log(colors.green(
+      `Info:${ALL_DEPENDENCIES.join(' ')} have installed.`
+    ));
+  }
+}
+
+function componentInit(root, compName) {
+  const templatePath = path.join(__dirname, 'template');
+  fs.copySync(templatePath, root);
+}
+
+function createComponent(compName) {
+  const root = path.resolve(compName);
+  checkCompName(compName);
+  mkdirOfComp(root, compName);
+  createPackageJsonFile(root, compName);
+  process.chdir(root);
+  checkNpmValid(root);
+  installDependcies(root);
+  componentInit(root, compName);
+}
+
+createComponent(componentName);
+
---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+/*
+*Check user's version of node, if it smaller than the leastVersion, exit and promt the error.
+*This file must work on node 0.10+.
+*/
+
+'use strict';
+
+const colors = require('colors');
+const leastVersion = 8;
+const userNodeVersion  = process.versions.node;
+const majorSemver = userNodeVersion.split('.')[0];
+
+if (majorSemver < leastVersion) {
+  console.error(
+    colors.red(
+      `Error:You are running Node ${userNodeVersion}.
+      create scrc component requires Node 8 or higher.
+      Please update your version of Node.`
+    )
+  );
+  process.exit();
+}
+
+require('./createScRcComponent');
